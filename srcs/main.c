ad5cd59d8db4e1914412d8abe1eb91cc7555be0f
Umut Sogukpinar <usogukpinar0@gmail.com>
2025-03-04 22:41:08 +0300

    update

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void	shell_loop(void)
+void	shell_loop(t_shell *shell)
 {
 	char	*input;
 	
@@ -15,7 +15,7 @@ void	shell_loop(void)
 		
 		printf("%s\n", input); // ? test
 		add_history(input); // ? do we need to use rl_clear_histroy for leaks ?
-		// process_input(input); // TODO: Implement this function
+		// process_input(shell, input); // TODO: Implement this function
 		free(input); // ? is this necessary
 	}
 	// TODO:
@@ -26,9 +26,27 @@ void	shell_loop(void)
 	// // Repeat until the user exits.
 }
 
+//* purpose: initialization of main structure (t_shell)
+
+t_shell	*init_shell(void)
+{
+	t_shell	*new;
+
+	new = ft_calloc(1, sizeof(t_shell));
+	if (!new)
+		return (NULL);
+	new->is_interactive = C_FALSE;
+	return (new);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
+	t_shell	*shell;
+
+	shell = init_shell();
+	if (!shell)
+		return (ALLOC_ERR); // ? special message for any error maybe?
 	setup_signals(); // TODO: Implement this function
-	shell_loop();
+	shell_loop(shell);
 	return (0);
 }
