209c05816bfb13b9845d85791299e4038d23b454
umutsogukpinar <usogukpinar0@gmail.com>
2025-05-14 20:11:07 +0300

    fix: echo builtin fix

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -1,14 +1,16 @@
 #include "minishell.h"
 
+static bool	check_flag(char *args);
+
 // * Function to echo arguments (with -n flag)
 int	ft_echo(char **args)
 {
-	int	i;
+	int		i;
 	bool	is_newline;
 
 	i = 1;
 	is_newline = false;
-	while (args[i] && are_strs_equal("-n", args[i]))
+	while (args[i] && check_flag(args[i]))
 	{
 		is_newline = true;
 		i++;
@@ -23,4 +25,23 @@ int	ft_echo(char **args)
 	if (!is_newline)
 		ft_putendl_fd("", 1);
 	return (EX_OK);
+}
+
+// Returns true if the argument is a valid -n flag (e.g., -n, -nn, -nnn...), false otherwise
+static bool	check_flag(char *args)
+{
+	int	i;
+
+	if (!(args && args[0] == '-' && args[1] == 'n'))
+	{
+		return (false);
+	}
+	i = 2;
+	while (args[i])
+	{
+		if (args[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
 }
