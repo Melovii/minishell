1dfb9dacd80098bb6ec5c2c1a6319115dea5f751
umutsogukpinar <usogukpinar0@gmail.com>
2025-05-14 20:12:07 +0300

    fix: exit builtin potential leak problem fixed

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -1,6 +1,7 @@
 #include "minishell.h"
 
 static bool	is_numeric(const char *str);
+static void	numeric_exit(t_shell *shell, char **args, int exit_code);
 static void custom_exit_msg(char *flag);
 
 // * Handles the exit command by checking arguments and shutting down the program
@@ -11,24 +12,33 @@ int	ft_exit(t_shell *shell, char **args)
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	if (!args[1])
 	{
-        shut_program(shell, NULL, shell->exit_flag);
+		ft_free_tab(args);
+        shut_program(shell, false, shell->exit_flag);
 	}
 	if (!is_numeric(args[1]))
 	{
 		custom_exit_msg(args[1]);
-        shut_program(shell, NULL, 2);
+		ft_free_tab(args);
+        shut_program(shell, false, 2);
 	}
 	if (args[2])
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
-		shell->exit_flag = 1;
-		return (1);
+		shell->exit_flag = EX_KO;
+		return (EX_KO);
 	}
 	exit_code = ft_atoi(args[1]) % 256;
+	numeric_exit(shell, args, exit_code);
+	return (0);
+}
+
+// Handles numeric exit: adjusts negative exit code, frees args, and exits shell
+static void	numeric_exit(t_shell *shell, char **args, int exit_code)
+{
 	if (exit_code < 0)
 		exit_code += 256;
-    shut_program(shell, NULL, exit_code);
-	return (0);
+	ft_free_tab(args);
+    shut_program(shell, false, exit_code);
 }
 
 // * Prints a custom error message when the exit argument is invalid
